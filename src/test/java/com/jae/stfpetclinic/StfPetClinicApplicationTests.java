67dc95dc209735bec6c209e76f0fb4b47f4e9566
bread <jyoo0821@gmail.com>
2019-08-07 21:55:52 -0400

    push push

---
+++ src/test/java/com/jae/stfpetclinic/StfPetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jae.stfpetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StfPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
